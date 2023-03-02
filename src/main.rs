e97e564d0670d5c9985a70456d12dbb909a4db02
Amogh Rijal <xamoghx@gmail.com>
2023-03-02 10:47:58 +0545

    Init postgres connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use postgres::{Client, Error, NoTls};
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect("", NoTls)?;
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS author (
+            id              SERIAL PRIMARY KEY,
+            name            VARCHAR NOT NULL,
+            country         VARCHAR NOT NULL
+            )
+    ",
+    )?;
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS book  (
+            id              SERIAL PRIMARY KEY,
+            title           VARCHAR NOT NULL,
+            author_id       INTEGER NOT NULL REFERENCES author
+            )
+    ",
+    )?;
+
+    Ok(())
 }
